8a3b6fb5eb73bcfef8376486317ac033aba590d3
Brant Faircloth <brant.faircloth@gmail.com>
2011-09-12 23:28:11 -0700

    ensure we return numpy.array from get_informative_sites

--- models_and_rates.py
+++ models_and_rates.py
@@ -124,7 +124,7 @@ def get_informative_sites(alignment, threshold=4):
         assert len(cells) == taxa.vector_size # should all have equal lengths
         for idx, cell in enumerate(cells):
             results[idx] += 1 if str(cell).upper() in "ATGC" else 0
-    return [results[x] >= threshold for x in sorted(results)]
+    return numpy.array([results[x] >= threshold for x in sorted(results)])
 
 def cull_uninformative_rates(rates, inform):
     """Zeroes out rates which are uninformative"""
