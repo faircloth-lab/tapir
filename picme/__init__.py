b048524397bd10cae743bbe5ebc1d2855b2cf703
Brant Faircloth <brant.faircloth@gmail.com>
2011-10-23 16:52:04 -0700

    update __init__ w/ metadata

--- picme/__init__.py
+++ picme/__init__.py
@@ -5,14 +5,44 @@ Author: Brant Faircloth
 Created by Brant Faircloth on 22 October 2011 14:10 PDT (-0700)
 Copyright (c) 2011 Brant C. Faircloth. All rights reserved.
 
-Description: 
-
 """
+
+__author__ = 'Brant C. Faircloth, Jonathan Chang, Mike E. Alfaro'
+__copyright__ = 'Copyright (c) 2011, Brant C. Faircloth, Jonathan Chang, Mike E. Alfaro'
+__credits__ = ['Brant Faircloth', 'Jonathan Chang','Mike Alfaro']
+__license__ = 'http://www.opensource.org/licenses/BSD-3-Clause'
+__version__ = '1.0'
+__maintainer__ = 'Brant Faircloth'
+__email__ = 'brant dot faircloth at gmail dot com'
+__status__ = 'testing'
+
+import sys
+
+if sys.version_info < (2, 7):
+    raise "must use Python 2.7 or greater" # pragma: no cover
+
+try: # pragma: no cover
+    import numpy
+    assert numpy.version.version > '1.3', "picme requires >= numpy 1.3"
+except ImportError: # pragma: no cover
+    raise ImportError, 'numpy does not seem to be installed. Please see the user guide.' # pragma: no cover
+
+try: # pragma: no cover
+    import scipy
+    assert scipy.version.version > '0.9', "picme requires >= scipy 0.9.0"
+except ImportError: # pragma: no cover
+    raise ImportError, 'scipy does not seem to be installed. Please see the user guide.' # pragma: no cover
+
+try:
+    import dendropy
+except ImportError: # pragma: no cover
+    raise ImportError, 'dendropy does not seem to be installed. Please see the user guide.' # pragma: no cover
+
 
 from db import *
 from base import *
 from compute import *
-from pkg_resources import resource_string
+from pkg_resources import resource_filename
 
 def get_hyphy_conf():
     return resource_filename(__name__, 'data/models_and_rates.bf')
