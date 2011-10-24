aee3ac7fa7fbe97cc0446e2c9b69cf53fbb92cd2
Brant Faircloth <brant.faircloth@gmail.com>
2011-10-23 18:01:41 -0700

    working setup.py with correct support file placement

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ import distribute_setup
 distribute_setup.use_setuptools()
 from setuptools import setup, find_packages
 
+import picme
 
 if __name__ == '__main__':
     setup(
         name="picme",
-        version="1.0",
-        description="phylogenetic informativeness calculator for marker evaluation",
+        version = picme.__version__,
+        description="phylogenetic informativeness calculator for marker evaluation (PICME)",
         author="Brant Faircloth, Jonathan Chang, Mike Alfaro",
         author_email="brant.faircloth+picme@gmail.com",
         url="http://github.com/faircloth-lab/picme/",
@@ -21,12 +22,21 @@ if __name__ == '__main__':
             "Programming Language :: Python",
             "Topic :: Scientific/Engineering :: Bio-Informatics",
              ],
-        requires=["dendropy","matplotlib","numpy","scipy","rpy2"],
+        requires=["dendropy","numpy(>=1.3)","scipy(>=0.9.0)"],
         long_description=open("README.rst").read(),
-        packages=[],
         scripts=["bin/picme_compute.py",
                 "bin/picme_plot.py",
                 "bin/picme_matplot.py"
-                ],
-        )
+            ],
+        packages=[
+            'picme',
+            'picme.tests',
+            ],
+        package_data = {
+            '':['*.txt'],
+            'tests':['test-data/*'],
+            'data':['*'],
+            },
+        include_package_data = True
+    )
 
